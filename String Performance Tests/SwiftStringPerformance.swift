2650ce14cb4e089cf25890725204f89d8ee753d3
Brian Nickel <bnickel@stackexchange.com>
2015-02-11 10:55:52 -0700

    Discovered ("~" as Character) is substantially slower than Character("~")

--- String Performance Tests/SwiftStringPerformance.swift
+++ String Performance Tests/SwiftStringPerformance.swift
@@ -10,13 +10,43 @@ import Foundation
 import XCTest
 
 class SwiftStringPerformance: XCTestCase {
+    
+    override class func setUp() {
+        super.setUp()
+        StringProvider.stringWithLength(100000, unicode: false) // Preload
+        sleep(2) // Attach breakpoint here, then
+    }
 
-    func testAsciiIteration() {
+    func testAsciiIterationWithImplicitType() {
         
         let text:String = StringProvider.stringWithLength(100000, unicode: false)
-        
         sleep(2)
+        self.measureBlock() {
+            for char in text {
+                if char == "~" {
+                    println(char)
+                }
+            }
+        }
+    }
+    
+    func testAsciiIterationWithExplicitType() {
         
+        let text:String = StringProvider.stringWithLength(100000, unicode: false)
+        sleep(2)
+        self.measureBlock() {
+            for char in text {
+                if char == "~" as Character {
+                    println(char)
+                }
+            }
+        }
+    }
+    
+    func testAsciiIterationWithInitializer() {
+        
+        let text:String = StringProvider.stringWithLength(100000, unicode: false)
+        sleep(2)
         self.measureBlock() {
             for char in text {
                 if char == Character("~") {
@@ -26,25 +56,17 @@ class SwiftStringPerformance: XCTestCase {
         }
     }
     
-    func testUnicodeIteration() {
-        let text:String = StringProvider.stringWithLength(100000, unicode: true)
+    func testAsciiIterationWithPreassignment() {
         
+        let text:String = StringProvider.stringWithLength(100000, unicode: false)
+        let reference = "~" as Character
         sleep(2)
-        
         self.measureBlock() {
             for char in text {
-                if char == Character("~") {
+                if char == reference {
                     println(char)
                 }
             }
         }
     }
-    
-    func XtestIteration() {
-        let text:String = StringProvider.stringWithLength(4, unicode: true)
-        for char in text {
-            println(join(",", map(String(char).unicodeScalars, { "\($0.value)" })))
-        }
-    }
-
 }
